8868e50acab87ea2b34245c05b635886c0fff04f
hedgehog <myregisg@gmail.com>
2023-10-05 00:46:50 +0300

    Completed ViewModel

---
+++ CustomControls/BindablePassword.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FilmFlow.CustomControls
+{
+    /// <summary>
+    /// Логика взаимодействия для BindablePassword.xaml
+    /// </summary>
+    public partial class BindablePassword : UserControl
+    {
+        public static readonly DependencyProperty PasswordProperty = DependencyProperty.Register("Password", typeof(SecureString), typeof(BindablePassword));
+        public SecureString Password
+        {
+            get { return (SecureString)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+        public BindablePassword()
+        {
+            InitializeComponent();
+            tbPasswordBox.PasswordChanged += OnPasswordChanged;
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = tbPasswordBox.SecurePassword;
+        }
+    }
+}
---
+++ Login/ViewModels/LoginViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FilmFlow.Login.ViewModels
+{
+    class LoginViewModel : ViewModelBase
+    {
+        private string _username { get; set; }
+        private SecureString _password { get; set; }
+        private string _errorMessage { get; set; }
+        private bool _isViewVisible = true;
+        
+        public string Username { get { return _username; } set { _username = value; OnPropertyChanged(nameof(Username)); } }
+        public SecureString Password { get { return _password; } set { _password = value; OnPropertyChanged(nameof(Password)); } }
+        public string ErrorMessage { get { return _errorMessage; } set { _errorMessage = value; OnPropertyChanged(nameof(ErrorMessage)); } }
+        public bool IsViewVisible { get { return _isViewVisible; } set { _isViewVisible = value; OnPropertyChanged(nameof(IsViewVisible)); } }
+
+        public ICommand LoginUser { get; }
+        public ICommand RecoverPassword { get; }
+        public ICommand ShowPassword { get; }
+
+        public LoginViewModel()
+        {
+            LoginUser = new ViewModelCommand(ExecuteLoginCommand, CanExecuteLogin);
+            RecoverPassword = new ViewModelCommand(RecoverPasswordCommand);
+        }
+
+        private void RecoverPasswordCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool CanExecuteLogin(object obj)
+        {
+            if(string.IsNullOrEmpty(Username) || Username.Length < 4 || Password == null || Password.Length < 6) 
+                return false;
+            return true;
+        }
+
+        private void ExecuteLoginCommand(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
