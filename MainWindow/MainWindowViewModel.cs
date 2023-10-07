44f2a8d31de6f4c956f39a092bbc24bcfba00fe0
hedgehog <myregisg@gmail.com>
2023-10-07 21:59:30 +0300

    #

--- MainWindow/MainWindowViewModel.cs
+++ MainWindow/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using FilmFlow.Models;
 using FilmFlow.ViewModels;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Windows.Controls;
 
 namespace FilmFlow.MainWindow.MainWindowModel
@@ -8,17 +9,20 @@ namespace FilmFlow.MainWindow.MainWindowModel
     class MainWindowViewModel : ViewModelBase
     {
         private ObservableCollection<MovieModel> _movies;
+        private int _selectedMovie = -1;
         public ObservableCollection<MovieModel> Movies { get { return _movies; } set { _movies = value; OnPropertyChanged(nameof(Movies)); } }
+        public int SelectedMovie { get { return _selectedMovie; } set { _selectedMovie = value; SelectedMovieChanged(); OnPropertyChanged(nameof(SelectedMovie)); } }
         MovieRepository MovieRepository { get; set; }
         public MainWindowViewModel()
         {
             Movies = new ObservableCollection<MovieModel>();
             MovieRepository = new MovieRepository();
+
             MovieRepository.LoadMovies(Movies);
         }
-        private void LoadMovies()
+        private void SelectedMovieChanged()
         {
-
+            
         }
     }
 }
