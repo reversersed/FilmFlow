c623c0bba106146e4df0df7015c4945d5d3682aa
hedgehog <myregisg@gmail.com>
2023-10-27 13:56:01 +0300

    #

--- CustomControls/VideoPlayer.xaml.cs
+++ CustomControls/VideoPlayer.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,30 +21,64 @@ namespace FilmFlow.CustomControls
     /// </summary>
     public partial class VideoPlayer : UserControl
     {
-        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register("SourceUrl", typeof(string), typeof(VideoPlayer));
-        public string SourceUrl { get { return (string)GetValue(SourceProperty); } 
-            set { SetValue(SourceProperty, value); } }
+        public static readonly DependencyProperty SourceProperty = DependencyProperty.Register("SourceUrl", typeof(string), typeof(VideoPlayer),
+            new PropertyMetadata(new PropertyChangedCallback(OnValueChanged)));
 
+        static Action loadVideo;
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => loadVideo?.Invoke();
+        public string SourceUrl { get { return (string)GetValue(SourceProperty); } 
+            set { SetValue(SourceProperty, value);  } }
+
+        //private Window fullscreen;
         public VideoPlayer()
         {
             InitializeComponent();
+            loadVideo = new Action(() => { 
+                VideoPlayerElement.Play();
+                VideoPlayerElement.Pause();
+            });
 
             VideoPlayerElement.LoadedBehavior = MediaState.Manual;
             VideoPlayerElement.UnloadedBehavior = MediaState.Manual;
             PlayButton.Click += PlayCommand;
+            SetEnabledStateToControls(false);
+            VideoPlayerElement.MediaOpened += VideoPlayerElement_MediaOpened;
+            VideoPlayerElement.MediaFailed += VideoPlayerElement_MediaFailed;
         }
 
+        private void VideoPlayerElement_MediaFailed(object? sender, ExceptionRoutedEventArgs e)
+        {
+            IconButtonPlay.Icon = FontAwesome.Sharp.IconChar.Play;
+            SetEnabledStateToControls(false);
+        }
 
+        private void VideoPlayerElement_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            SetEnabledStateToControls(true);
+        }
+        private void SetEnabledStateToControls(bool enabled)
+        {
+            PlayButton.IsEnabled = enabled;
+        }
         private void PlayCommand(object sender, RoutedEventArgs e)
         {
+            if (!VideoPlayerElement.HasVideo)
+                return;
             if(IconButtonPlay.Icon == FontAwesome.Sharp.IconChar.Play)
             {
                 IconButtonPlay.Icon = FontAwesome.Sharp.IconChar.Pause;
                 VideoPlayerElement.Play();
+
+                //(this.Parent as StackPanel).Children.Remove(this);
+                //fullscreen = new Window();
+                //fullscreen.Content = this;
+                //fullscreen.WindowStyle = WindowStyle.None;
+                //fullscreen.WindowState = WindowState.Maximized;
+                //fullscreen.ResizeMode = ResizeMode.NoResize;
+                //fullscreen.Show();
             }
             else
             {
-
                 IconButtonPlay.Icon = FontAwesome.Sharp.IconChar.Play;
                 VideoPlayerElement.Pause();
             }
