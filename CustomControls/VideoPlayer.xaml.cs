e7557d463a0a9ce9d12ec3ffc87423886c5baf29
hedgehog <myregisg@gmail.com>
2023-10-28 01:51:15 +0300

    Made video player

--- CustomControls/VideoPlayer.xaml.cs
+++ CustomControls/VideoPlayer.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace FilmFlow.CustomControls
 {
@@ -23,12 +13,16 @@ namespace FilmFlow.CustomControls
     {
         public static readonly DependencyProperty SourceProperty = DependencyProperty.Register("SourceUrl", typeof(string), typeof(VideoPlayer),
             new PropertyMetadata(new PropertyChangedCallback(OnValueChanged)));
+        public static readonly DependencyProperty elapsedProperty = DependencyProperty.Register("ElapsedTime", typeof(string), typeof(VideoPlayer));
 
         static Action loadVideo;
         private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) => loadVideo?.Invoke();
         public string SourceUrl { get { return (string)GetValue(SourceProperty); } 
             set { SetValue(SourceProperty, value);  } }
+        public string ElapsedTime { get { return (string)GetValue(elapsedProperty); } set { SetValue(elapsedProperty, value); } }
 
+        private double savedVolume;
+        private DispatcherTimer videoTimer;
         //private Window fullscreen;
         public VideoPlayer()
         {
@@ -41,6 +35,8 @@ namespace FilmFlow.CustomControls
             VideoPlayerElement.LoadedBehavior = MediaState.Manual;
             VideoPlayerElement.UnloadedBehavior = MediaState.Manual;
             PlayButton.Click += PlayCommand;
+            ElapsedTime = "00:00:00/00:00:00";
+            iconVolume.Icon = FontAwesome.Sharp.IconChar.VolumeMute;
             SetEnabledStateToControls(false);
             VideoPlayerElement.MediaOpened += VideoPlayerElement_MediaOpened;
             VideoPlayerElement.MediaFailed += VideoPlayerElement_MediaFailed;
@@ -48,17 +44,47 @@ namespace FilmFlow.CustomControls
 
         private void VideoPlayerElement_MediaFailed(object? sender, ExceptionRoutedEventArgs e)
         {
+            videoTimer.Stop();
+            ElapsedTime = "00:00:00/00:00:00";
+            iconVolume.Icon = FontAwesome.Sharp.IconChar.VolumeMute;
+            VolumeSlider.Value = 0;
             IconButtonPlay.Icon = FontAwesome.Sharp.IconChar.Play;
             SetEnabledStateToControls(false);
         }
 
         private void VideoPlayerElement_MediaOpened(object sender, RoutedEventArgs e)
         {
+            TimeSlider.Maximum = VideoPlayerElement.NaturalDuration.TimeSpan.TotalSeconds;
+            VolumeSlider.Maximum = 1;
+            VolumeSlider.Value = VideoPlayerElement.Volume;
+            VolumeChange(VideoPlayerElement.Volume);
+            videoTimer = new DispatcherTimer();
+            videoTimer.Interval = TimeSpan.FromSeconds(1);
+            videoTimer.Tick += VideoTimer_Tick;
+            videoTimer.Start();
+            ElapsedTime = string.Format("{0}/{1}", VideoPlayerElement.Position.ToString(@"hh\:mm\:ss"), VideoPlayerElement.NaturalDuration.TimeSpan.ToString(@"hh\:mm\:ss"));
             SetEnabledStateToControls(true);
         }
+
+        private void VideoTimer_Tick(object? sender, EventArgs e)
+        {
+            if (!VideoPlayerElement.HasVideo || !VideoPlayerElement.NaturalDuration.HasTimeSpan)
+            {
+                videoTimer.Stop();
+                return;
+            }
+            if (VideoPlayerElement.NaturalDuration.TimeSpan.TotalSeconds > 0 && VideoPlayerElement.Position.TotalSeconds > 0)
+                TimeSlider.Value = VideoPlayerElement.Position.TotalSeconds;
+            VolumeSlider.Value = VideoPlayerElement.Volume;
+            ElapsedTime = string.Format("{0}/{1}", VideoPlayerElement.Position.ToString(@"hh\:mm\:ss"), VideoPlayerElement.NaturalDuration.TimeSpan.ToString(@"hh\:mm\:ss"));
+        }
+
         private void SetEnabledStateToControls(bool enabled)
         {
             PlayButton.IsEnabled = enabled;
+            TimeSlider.IsEnabled = enabled;
+            VolumeSlider.IsEnabled = enabled;
+            volumeButton.IsEnabled = enabled;
         }
         private void PlayCommand(object sender, RoutedEventArgs e)
         {
@@ -83,5 +109,47 @@ namespace FilmFlow.CustomControls
                 VideoPlayerElement.Pause();
             }
         }
+
+        private void TimeSlider_ValueChanged(object sender, MouseButtonEventArgs e)
+        {
+            if (VideoPlayerElement.NaturalDuration.TimeSpan.TotalSeconds > 0)
+                VideoPlayerElement.Position = TimeSpan.FromSeconds(TimeSlider.Value);
+        }
+
+        private void TimeSlider_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (VideoPlayerElement.NaturalDuration.TimeSpan.TotalSeconds > 0)
+            {
+                TimeSlider.Value += Math.Sign(e.Delta) * 5;
+                VideoPlayerElement.Position = TimeSpan.FromSeconds(TimeSlider.Value);
+            }
+        }
+        private void VolumeSlider_ValueChanged(object sender, MouseButtonEventArgs e) => VolumeChange(VolumeSlider.Value);
+
+        private void VolumeSlider_MouseWheel(object sender, MouseWheelEventArgs e) => VolumeChange(VolumeSlider.Value+ Math.Sign(e.Delta)*0.1);
+        private void VolumeChange(double value)
+        {
+            if (value > 0.4)
+                iconVolume.Icon = FontAwesome.Sharp.IconChar.VolumeUp;
+            else if (value > 0.01)
+                iconVolume.Icon = FontAwesome.Sharp.IconChar.VolumeDown;
+            else
+            {
+                savedVolume = VideoPlayerElement.Volume;
+                iconVolume.Icon = FontAwesome.Sharp.IconChar.VolumeMute;
+            }
+            VideoPlayerElement.Volume = value;
+            VolumeSlider.Value = value;
+        }
+
+        private void volumeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(VideoPlayerElement.Volume > 0)
+                VolumeChange(0);
+            else
+                VolumeChange(savedVolume);
+        }
+
+        private void VideoPlayerElement_MouseLeftButtonDown(object sender, MouseButtonEventArgs e) => PlayCommand(sender, null);
     }
 }
