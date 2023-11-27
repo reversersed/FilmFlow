eee67a33c72978b6bbd558b787a2ea35cecc3a38
hedgehog <myregisg@gmail.com>
2023-11-28 00:35:34 +0300

    #

--- Models/MovieRepository.cs
+++ Models/MovieRepository.cs
@@ -27,6 +27,7 @@ namespace FilmFlow.Models
                                             .Include(e => e.Genre)
                                                 .ThenInclude(i => i.Genre)
                                             .Select(i => i)
+                                            .Where(i => db.reviews.Where(x => x.MovieId == i.Id).Where(x => (DateTime.UtcNow.ToUniversalTime() - x.WriteDate.ToUniversalTime()).Days < days).Any())
                                             .OrderByDescending(x => db.reviews.Include(f => f.Movie).Where(i => i.Movie.Id == x.Id).Where(i => (DateTime.UtcNow.ToUniversalTime()-i.WriteDate.ToUniversalTime()).Days < days).Count())
                                             .Take(30)
                                             .ToList())
